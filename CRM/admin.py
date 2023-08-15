98abea1c3e7d3092157801bc149bdfb6c6a2d7b5
Dmitriy Nikonov <NowReborngg@gmail.com>
2023-08-16 00:04:48 +0300

    Delete CRM directory

--- CRM/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Client)
-admin.site.register(Payment)
-admin.site.register(Teacher)
-admin.site.register(Lesson)
-admin.site.register(Phone)
-admin.site.register(Group)
